669fecbf080f80d9a34c6b61eec85ec1ddadc026
Hyungjun <hpark416@gatech.edu>
2020-12-05 14:36:59 -0500

    BMED 4699 Semester End Research Report
    12/5/2020 - FALL 2020
    -Missing the Arduino Code
    INCLUDES:
    -GUI py
    -Circle Task py
    -Calibration py
    -Excel Writer
    
    Co-Authored-By: keauliib <70108944+keauliib@users.noreply.github.com>
    Co-Authored-By: manishbhatta <71843337+manishbhatta@users.noreply.github.com>

---
+++ LATEST CODES/Calibration.py
@@ -0,0 +1,63 @@
+import serial
+import time
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation 
+import numpy as np 
+
+arduinoData=serial.Serial('COM15',115200)
+time.sleep(1)
+LC1 = 0
+lst = []
+lst2 = []
+start_time = time.time()
+seconds = 7  
+kflag = 0
+counter = 1
+trialnum = 1
+
+while True:
+    dataPacket=arduinoData.readline()
+    dataPacket=str(dataPacket,'utf-8')
+    splitPacket=dataPacket.split(',')
+    LC1=int(splitPacket[0])
+    LC2=int(splitPacket[1])
+    Force=int(splitPacket[2])
+    elapsed_time = 0
+    start_time = time.time()
+    flag = True
+    if LC2 > 1000 and elapsed_time == 0 and kflag < 3:
+        start_time = time.time()
+        while flag:
+            if elapsed_time > seconds:
+                maxval = max(lst)
+                print("////////////// Trial ",trialnum," ended with max force of: ", int(maxval)," lbs")
+                flag = False
+                kflag += 1
+                elapsed_time = 0
+                lst = []
+                lst2.append(maxval)
+                maxval = 0
+                counter=1
+                trialnum+=1
+
+            else:
+                dataPacket=arduinoData.readline()
+                dataPacket=str(dataPacket,'utf-8')
+                splitPacket=dataPacket.split(',')
+                LC1=int(splitPacket[0])
+                LC2=int(splitPacket[1])
+                Force=int(splitPacket[2])
+                #Time Grace Period = 7 seconds
+                current_time = time.time()
+                elapsed_time = current_time - start_time
+                lst.append(Force)
+                print("Trial ",trialnum," elapsed_time: ",elapsed_time)
+                print('LbForce: ',Force) #'Led Brightness=',ledI   ##'X=',X
+
+    else: 
+        if kflag == 3:
+            print("////////////// Calibrated Avg Force of 3 Trials: ", sum(lst2)/3, " lbs")
+            break
+        if counter == 1:
+            print("////////////// Press START to begin calibration of trial ",trialnum,"/3",)
+            counter+=1
