4714d1b2c3f1d074ed9d8d381c1a11df600afff3
H Park <71608209+hpark416@users.noreply.github.com>
2021-02-08 16:07:22 -0500

    Add files via upload
    Fixed lag of plotting, Decreased resolution of inputs, And adjusted the program task paradigm to function with actual time instead of arbitrary loop intervals (which can vary with strain on processing computer aka computer lag can increase the time that they would need to hold it)

--- SWARM17.py
+++ SWARM17.py
@@ -1,117 +1,93 @@
 import serial
 import time
-import matplotlib.pyplot as plt 
-import matplotlib.animation as animation
+import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import numpy as np 
-import random 
-import multiprocessing
-
+import numpy as np
+import random
 from typing import Tuple
 
 finAvg = 25
-analogThreshold = 1000
+analogThreshold = 1024
+# def get_calibrated():
+# WORK ON TRANSFERING CALIBRATION OVER
 
 
 def get_target_values():
-    target_value = random.uniform(6,20)
+    target_value = random.uniform(6, 20)
     target_value_upper = target_value + 2
     target_value_lower = target_value - 2
-    #the circle to plot
-
-    # upper range
-    # r_upper = target_value_upper
-    # th = np.linspace(0,2 * np.pi, num=100)
-    # x_upper = r_upper * np.cos(th)
-    # y_upper = r_upper * np.sin(th)
-    # # lower range
-    # r_lower = target_value_lower
-    # x_lower = r_lower * np.cos(th)
-    # y_lower = r_lower * np.sin(th)
-
     if target_value_upper > 25:
         target_value_upper = 25
         target_value_lower = 21
-    print("Target Value: ", target_value_upper-2)
-
+    print("Target Value: ", target_value_upper - 2)
     return target_value_lower, target_value_upper
 
 
 def get_circle_values(radius: int) -> Tuple[float, float]:
-    th = np.linspace(0,2 * np.pi, num=100)
+    th = np.linspace(0, 2 * np.pi, num=100)
     return radius * np.cos(th), radius * np.sin(th)
 
 
-#pull in data
-arduinoData=serial.Serial('COM5',115200)
+# pull in data
+arduinoData = serial.Serial('COM5', 115200)
 time.sleep(1)
-# LC2 = 0
-# start_time_total = time.time()
-# start_time = time.time() 
 
 
-def readArduino() -> Tuple[int, int, int]:
-    # global LC1, LC2, Force
-    dataPacket=arduinoData.readline()
-    dataPacket=str(dataPacket,'utf-8')
-    splitPacket=[int(x) for x in dataPacket.split(',')]
-    return splitPacket[:3]  # LC1, LC2, Force
-    # LC1=int(splitPacket[0])
-    # LC2=int(splitPacket[1]) # button -- analog read in interval: [0, 2^10)
-    # Force=int(splitPacket[2])
+def readArduino() -> Tuple[int, int, int, int]:
+    dataPacket = arduinoData.readline()
+    dataPacket = str(dataPacket, 'utf-8')
+    splitPacket = [int(x) for x in dataPacket.split(',')]
+    return splitPacket[:4]  # LLC, Start, LbfForce, LCP1
 
 
-timeE = []
-count = 0
+Force = 0
+c = 1
+timeE = time.time()
+timeEntered = []
 target_value_lower, target_value_upper = None, None
 
+
 def animate(i):
-    global timeE, count, target_value_lower, target_value_upper
-    th = np.linspace(0,2 * np.pi,num=100)
-    dataPacket=arduinoData.readline()
-    dataPacket=str(dataPacket,'utf-8')
-    splitPacket=dataPacket.split(',')
-    r = int(splitPacket[2])
+    global timeE, target_value_lower, target_value_upper, Force, c, timeEntered, end_time
+    LLC, Start, Force, LCP1 = readArduino()
+    th = np.linspace(0, 2 * np.pi, num=100)
+    r = int(Force)
     x = r * np.cos(th)
     y = r * np.sin(th)
     plt.cla()
-    plt.plot(x,y)
+    plt.title(str(Force))
+    plt.plot(x, y)  # check out
+    plt.plot(1, i)
     x_lower, y_lower = get_circle_values(target_value_lower)
     x_upper, y_upper = get_circle_values(target_value_upper)
-    plt.plot(x_upper,y_upper)
-    plt.plot(x_lower,y_lower)
+    plt.plot(x_upper, y_upper)
+    plt.plot(x_lower, y_lower)
     plt.axis('equal')
-    LC1, LC2, Force = readArduino()
-
-    if target_value_lower < Force < target_value_upper:        
-        print("YAY")
-        timeEntered = time.time()
-        timeE = np.append(timeE,timeEntered)
-        print("///////////////////////////: ",Force, "Held Time: ",count)
-        count = count + 1
-        if count == 15:
-            print("You passed! Time taken was ",count)
+    if target_value_lower < Force < target_value_upper:
+        if c == 1:
+            timeEntered = time.time()
+            c = 0
+        timeE = time.time()
+        print("GOOD", Force, timeE, timeEntered, timeE - timeEntered)
+        if timeE-timeEntered >= 5:
             plt.close()
+            end_time = time.time()
     else:
-        print("OOF")
-        timeE = []
-        count = 0
-        
+        print("not in range", Force, timeE)
+        c = 1
+
 
 def input_simulator():
-    global count, target_value_lower, target_value_upper
+    global target_value_lower, target_value_upper, end_time
     start_time = time.time()
     target_value_lower, target_value_upper = get_target_values()
-    ani = FuncAnimation(plt.gcf(), animate, interval=50)
+    ani = FuncAnimation(plt.gcf(), animate, frames=np.arange(0, 100, 1), interval=100)
     plt.tight_layout()
     plt.show()
-    end_time = time.time()
+    print("You passed! Time taken was ", str(end_time - start_time))
     return end_time - start_time
-    # plt.show(block=False)
-    # while count < 15:
-    #     print(count)
-    # plt.close()
 
 
 if __name__ == '__main__':
     input_simulator()
+
